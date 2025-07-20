226e7e151e78c0f6cd87ddb323a6f6f78c89971d
Mathias Atugonza <atumathias20@gmail.com>
2025-07-20 09:40:44 +0300

    added

--- lib/services/firebase_messaging_service.dart
+++ lib/services/firebase_messaging_service.dart
@@ -2,6 +2,7 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
+
 class FirebaseMessagingService {
   static final FirebaseMessaging _messaging = FirebaseMessaging.instance;
 
